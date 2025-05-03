23437ab86fb4dcf6702cd1297abeb3e4fc66c270
berrrchik <berrrchik@mail.ru>
2025-05-03 22:28:46 +0300

    feat: add LearningState to track flashcards learning progress

--- TestMe/Model/Flashcard.swift
+++ TestMe/Model/Flashcard.swift
@@ -7,11 +7,30 @@ struct Flashcard: Identifiable, Codable, Equatable {
     var categoryId: UUID
     var isLearned: Bool
     
+    var learningState: LearningState
+    var lastReviewed: Date?
+    var nextReviewDate: Date?
+    var repetitionCount: Int
+    var easeFactor: Double
+    
     init(id: UUID = UUID(), term: String, definition: String, categoryId: UUID, isLearned: Bool = false) {
         self.id = id
         self.term = term
         self.definition = definition
         self.categoryId = categoryId
         self.isLearned = isLearned
+        
+        self.learningState = .new
+        self.lastReviewed = nil
+        self.nextReviewDate = nil
+        self.repetitionCount = 0
+        self.easeFactor = 2.5
     }
+}
+
+enum LearningState: String, Codable {
+    case new
+    case learning
+    case reviewing
+    case mastered 
 } 
