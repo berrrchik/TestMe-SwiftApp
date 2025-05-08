200e037b0be6014af6a6ada9cb842eac9cb1b48b
berrrchik <berrrchik@mail.ru>
2025-05-08 22:50:10 +0300

    feat: add constants for UserDefaults storage keys

---
+++ TestMe/Model/UserDefaultsKeys.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+enum UserDefaultsKeys {
+    static let categories = "categories"
+    static let flashcards = "flashcards"
+    static let quizResults = "quizResults"
+    static let learningSettings = "learningSettings"
+    static let lastLearningSessionDate = "lastLearningSessionDate"
+    static let dailyLearningStats = "dailyLearningStats"
+} 
